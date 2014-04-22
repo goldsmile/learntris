3e8007f2318d2bea76b94d443d4e946bf2965ac6
proton lpmc <lpmc@proton.sabren.com>
2014-04-22 06:38:57 -0400

    web/leantris.ts: show the whole matrix (in random colors)

--- web/learntris.ts
+++ web/learntris.ts
@@ -1,27 +1,52 @@
+var gap = 1;
+var csize = 16;
+var gridw = 10;
+var gridh = 22;
+
+function randcolor() : number {
+    return Math.floor(Math.random() * 7);
+}
+
 class LearntrisConsole {
-    
+
+
     constructor() {
-	this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { 
-	    preload: this.preload, create: this.create });
+	this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
+	    preload: this.preload,
+	    create: this.create,
+	    update: this.update,
+	});
     }
-    
+
     game: Phaser.Game;
-    blocks: number[];
+    blocks: Phaser.Sprite[][];
 
     preload() {
 	this.game.load.spritesheet('blocks', 'assets/blocks.png', 16, 16);
     }
 
     create() {
-	this.blocks = new Array(8);
-	var x = this.game.world.centerX - (7 * 16);
-	var y = this.game.world.centerY - 8;
-	for (var i = 0; i < 7; ++i) {
-	    this.blocks[i] = this.game.add.sprite(x, y, 'blocks', i);
-	    x += 32;
-      }
+	this.blocks = new Array(gridh);
+	var y = this.game.world.centerY - (gridh/2 * (csize+gap));
+	for (var j = 0; j < gridh; ++j) {
+	    this.blocks[j] = new Array(gridw);
+	    var x = this.game.world.centerX - (gridw/2 * (csize+gap));
+	    for (var i = 0; i < gridw; ++i) {
+		this.blocks[j][i]
+		    = this.game.add.sprite(x, y, 'blocks', randcolor());
+		x += csize + gap;
+	    }
+	    y += csize + gap;
+	}
     }
-    
+
+    update() {
+	for (var j = 0; j < gridh; ++j) for (var i = 0; i < gridw; ++i) {
+	    var block = this.blocks[j][i];
+	    block.frame = randcolor();
+	}
+    }
+
 }
 
 window.onload = () => {
